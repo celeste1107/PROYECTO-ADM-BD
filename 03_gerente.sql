817bcd0b4e3b9c7ae57a2ff5385ee947129f3b97
María Fernanda Bolaños Carmona. <fernanda2121.mfbo64@gmail.com>
2026-06-28 17:54:23 -0600

    Update 03_gerente.sql

--- 03_gerente.sql
+++ 03_gerente.sql
@@ -1,1 +1,25 @@
+---Consulta de tablas por gerente----
+
+
+
+select *from clientes;
+
+select *from empleados;
+
+select *from proveedores;
+
+select *from inventario;
+
+select *from ordenes_compra;
+
+select *from proyecto;
+
+select *from maquinaria;
+
+select *from asignacion_proyecto;
+
+select *from asignacion_maquinaria;
+
+select *from control_combustible;
+
 select * from detalle_orden_compra;
